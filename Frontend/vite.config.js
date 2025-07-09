5361da3107a0c3ca584fe0fb9a1062b4c1cce0a5
pappu123k <141868014+pappu123k@users.noreply.github.com>
2025-07-09 17:19:47 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
+      '/api': 'https://codeoptima-backend-1wq3.onrender.com'
     }
   }
 })
